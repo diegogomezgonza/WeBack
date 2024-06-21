d9c3dc9290b8659db6fe559c18b5f97937a14899
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-06-21 21:05:27 +0200

    Configuración global

--- src/main/java/com/example/weback/demo/DemoApplication.java
+++ src/main/java/com/example/weback/demo/DemoApplication.java
@@ -1,8 +1,16 @@
 package com.example.weback.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Configuración global para el proyecto
+@EnableAutoConfiguration
+@Configuration
+//Detección automática de beans
+@ComponentScan("com.example.weback")
 @SpringBootApplication
 public class DemoApplication {
 
