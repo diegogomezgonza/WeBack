6ff8d7feb444ed10ed44e8a327b36c5fb58ba56f
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-06-23 13:43:18 +0200

    Thymeleaf

--- src/main/java/com/example/weback/controllers/DemoController.java
+++ src/main/java/com/example/weback/controllers/DemoController.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     
-    //Mapeo de la URL 
+    //URL index
     @GetMapping("/index.html")
     public String index() {
         return "Return de prueba";
     }
+
+    //URL home
+    @GetMapping(value="/home.html")
+    public String home(){
+    return "home.html";
+}
 }
