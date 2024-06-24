cacfef44c610ff35eba455838d29e9a6d3b7144d
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-06-24 15:14:54 +0200

    Fix css + configuración de fuentes raleway y lilita one

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: ["./src/main/resources/**/*.{html,js}"],
+  content: ["./src/main/resources/**/*.{html,js}",
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway:['Raleway', 'sans-serif'],
+        lilita: ['Lilita One', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
