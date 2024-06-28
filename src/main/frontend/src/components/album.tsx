4f3c4f2a3f487656aee3c1c90b397b504109779e
diegogomezgonza <diegogomezgonza9@gmail.com>
2024-06-28 11:07:59 +0200

    img name on  hover

--- src/main/frontend/src/components/album.tsx
+++ src/main/frontend/src/components/album.tsx
@@ -7,50 +7,90 @@ const Album: React.FC = () => {
         <div className="album">
           <div className="responsive-container-block bg">
             <div className="responsive-container-block img-cont">
-              <img
-                className="img"
-                src="https://picsum.photos/600/2000?random=6"
-                alt="Gallery Image"
-              />
+              <div className="img-wrapper">
+                <img
+                  className="img"
+                  src="https://picsum.photos/600/2000?random=6"
+                  alt="Gallery Image"
+                />
+                <div className="img-overlay">
+                  <span className="img-title">Image 1</span>
+                </div>
+              </div>
             </div>
             <div className="responsive-container-block img-cont">
-              <img
-                className="img"
-                src="https://picsum.photos/1600/2400?random=2"
-                alt="Gallery Image"
-              />
-              <img
-                className="img img-last"
-                src="https://picsum.photos/2400/1000?random=5"
-                alt="Gallery Image"
-              />
-              <img
-                className="img"
-                src="https://picsum.photos/1400/1600?random=7"
-                alt="Gallery Image"
-              />
+              <div className="img-wrapper">
+                <img
+                  className="img"
+                  src="https://picsum.photos/1600/2400?random=2"
+                  alt="Gallery Image"
+                />
+                <div className="img-overlay">
+                  <span className="img-title">Image 2</span>
+                </div>
+              </div>
+              <div className="img-wrapper">
+                <img
+                  className="img img-last"
+                  src="https://picsum.photos/2400/1000?random=5"
+                  alt="Gallery Image"
+                />
+                <div className="img-overlay">
+                  <span className="img-title">Image 3</span>
+                </div>
+              </div>
+              <div className="img-wrapper">
+                <img
+                  className="img"
+                  src="https://picsum.photos/1400/1600?random=7"
+                  alt="Gallery Image"
+                />
+                <div className="img-overlay">
+                  <span className="img-title">Image 4</span>
+                </div>
+              </div>
             </div>
             <div className="responsive-container-block img-cont">
-              <img
-                className="img img-last"
-                src="https://picsum.photos/1200/600?random=3"
-                alt="Gallery Image"
-              />
-              <img
-                className="img"
-                src="https://picsum.photos/2800/800?random=8"
-                alt="Gallery Image"
-              />
-              <img
-                className="img"
-                src="https://picsum.photos/1400/1600?random=9"
-                alt="Gallery Image"
-              />
-              <img
-                className="img"
-                src="https://picsum.photos/1400/1600?random=10"
-                alt="Gallery Image"
-              />
+              <div className="img-wrapper">
+                <img
+                  className="img img-last"
+                  src="https://picsum.photos/1200/600?random=3"
+                  alt="Gallery Image"
+                />
+                <div className="img-overlay">
+                  <span className="img-title">Image 5</span>
+                </div>
+              </div>
+              <div className="img-wrapper">
+                <img
+                  className="img"
+                  src="https://picsum.photos/2800/800?random=8"
+                  alt="Gallery Image"
+                />
+                <div className="img-overlay">
+                  <span className="img-title">Image 6</span>
+                </div>
+              </div>
+              <div className="img-wrapper">
+                <img
+                  className="img"
+                  src="https://picsum.photos/1400/1600?random=9"
+                  alt="Gallery Image"
+                />
+                <div className="img-overlay">
+                  <span className="img-title">Image 7</span>
+                </div>
+              </div>
+              <div className="img-wrapper">
+                <img
+                  className="img"
+                  src="https://picsum.photos/1400/1600?random=10"
+                  alt="Gallery Image"
+                />
+                <div className="img-overlay">
+                  <span className="img-title">Image 8</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
