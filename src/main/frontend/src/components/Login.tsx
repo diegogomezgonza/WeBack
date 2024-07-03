d810d751adb61034045f97d4a9ea2182ad65f279
diegogomezgonza <diegogomezgonza9@gmail.com>
2024-07-03 20:38:01 +0200

    Modal de Login

--- src/main/frontend/src/components/Login.tsx
+++ src/main/frontend/src/components/Login.tsx
@@ -1,28 +1,27 @@
-import React, { useState } from "react";
-
+import React from "react";
 
 const Login: React.FC = () => {
   return (
-    //Modal de login
+    // Modal de login
     <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
       <div className="bg-white p-8 rounded shadow-md w-96">
         <h2 className="text-2xl mb-6 text-center">Login</h2>
         <form>
-            {/* Correo */}
+          {/* Correo */}
           <div className="mb-4">
             <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-2">Correo Electrónico</label>
-            <input 
-              type="email" 
-              id="email" 
+            <input
+              type="email"
+              id="email"
               className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
           {/* Contraseña */}
           <div className="mb-6">
             <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-2">Contraseña</label>
-            <input 
-              type="password" 
-              id="password" 
+            <input
+              type="password"
+              id="password"
               className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
--- src/main/frontend/src/components/header.tsx
+++ src/main/frontend/src/components/header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 //Componentes
@@ -6,6 +6,17 @@ import Login from "./Login";
 
 //Navbar
 const Header: React.FC = () => {
+  //El estado inicial del modal es false para que no se muestre
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  //Abrir componente Login al hacer click en contact
+  const handleContactClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    //Hago preventDefault ya que sino haría redirect a /contact
+    event.preventDefault();
+    //Cuando se ha hecho click, el estado del modal pasa a ser true para que se muestre
+    setIsModalOpen(true);
+  };
+
   return (
     <header className="bg-white">
       <div className="container mx-auto px-4 py-6 flex justify-between items-center">
@@ -21,13 +32,14 @@ const Header: React.FC = () => {
             </li>
             <li>
               {/* Perfíl */}
-              <Link to="/contact">
+              <Link to="/contact" onClick={handleContactClick}>
                 <img src={`contact.png`} alt="Contacto" className="h-10 w-10" />
               </Link>
             </li>
           </ul>
         </nav>
       </div>
+      {isModalOpen && <Login />}
     </header>
   );
 };
