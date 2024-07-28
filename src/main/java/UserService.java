61a860efd5ae9243e877958eef0c4785228f9bbd
diegogomezgonza <diegogomezgonza9@gmail.com>
2024-07-28 20:17:23 +0200

    UserService para backend

---
+++ src/main/java/UserService.java
@@ -0,0 +1,25 @@
+import com.example.weback.app.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
